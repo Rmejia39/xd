0d77ab2f66c9084744f41c4a93b5ce57b02677bb
Rmejia39 <sortomejia66@gmail.com>
2023-03-17 18:23:55 -0600

    cambios

--- docentes.js
+++ docentes.js
@@ -25,6 +25,11 @@ Vue.component('component-docentes',{
                 this.docentes.splice(index,1);
             }
             localStorage.setItem("docentes", JSON.stringify(this.docentes) );
+            fetch(`private/modulos/docentes/docentes.php?accion=${this.accion}&docentes=${JSON.stringify(this.docente)}`)
+            .then(resp=>resp.json())
+            .then(resp=>{
+                console.log(resp);
+            });
             this.nuevoDocente();
         },
         eliminarDocente(docente){
@@ -47,6 +52,14 @@ Vue.component('component-docentes',{
         listar(){
             this.docentes = JSON.parse( localStorage.getItem('docentes') || "[]" )
                 .filter(docente=>docente.nombre.toLowerCase().indexOf(this.buscar.toLowerCase())>-1);
+            if( this.docentes.length<=0 && this.buscar.trim().length<=0 ){
+                fetch('private/modulos/docentes/docentes.php?accion=consultar')
+                .then(resp=>resp.json())
+                .then(resp=>{
+                    this.docentes = resp;
+                    localStorage.setItem("docentes", JSON.stringify(this.docentes) );
+                });
+            }
         }
     },
     template: `
